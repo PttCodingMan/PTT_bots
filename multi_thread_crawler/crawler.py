ab98938789a2c6560a211eb21c11d909690b7866
CodingMan <CodingMan@uptt.cc>
2020-07-28 10:59:07 +0800

    Adjust crawler

--- multi_thread_crawler/crawler.py
+++ multi_thread_crawler/crawler.py
@@ -1,10 +1,11 @@
 import threading
 import json
 from PyPtt import PTT
+import os
 
 
 class Crawler:
-    def __init__(self, account_file_name, board, start_index, end_index, number_threads: int = 5):
+    def __init__(self, account_file_name, board, start_index, end_index, number_threads: int = 6):
         with open(account_file_name) as f:
             account = json.load(f)
 
@@ -68,21 +69,78 @@ class Crawler:
         ptt_bot.login(id, pw)
 
         for index in range(start_index, end_index + 1):
-            # print(index)
-            post_info = ptt_bot.get_post(board, post_index=index)
+            try:
+                post_info = ptt_bot.get_post(board, post_index=index)
+            except:
+                print('get_post error', index)
+                continue
+            if post_info is None:
+                continue
 
+            if post_info.date is None:
+                print('aid', post_info.aid)
+                print('內文', post_info.content)
+                print('===========')
+                continue
+
+            # print(post_info.title)
+            try:
+                date = post_info.date.split(' ')
+                date.pop(0)
+                date.insert(0, date[-1])
+                date.pop()
+                date = [x for x in date if len(x) != 0]
+                if len(date[2]) == 1:
+                    date[2] = '0' + date[2]
+                file_name = '_'.join(date).replace(':', '.') + '.json'
+
+                if len(file_name) != 25:
+                    print('aid', post_info.aid)
+                    print('file name error', file_name)
+                    print('date error', post_info.date)
+                    print('===========')
+
+                    continue
+            except:
+                print('aid', post_info.aid)
+                print('date error', post_info.date)
+                print('===========')
+                continue
+
+            data = dict()
+            data['aid'] = post_info.aid
+            data['author'] = post_info.author
+            data['title'] = post_info.title
+            data['date'] = post_info.date
+            data['location'] = post_info.location
+            data['content'] = post_info.content
+            data['ip'] = post_info.ip
+
+            push_list = list()
+            for push in post_info.push_list:
+                current_push = dict()
+                current_push['author'] = push.author
+                current_push['content'] = push.content
+                current_push['time'] = push.time
+                push_list.append(current_push)
+
+            data['push_list'] = push_list
+
+            if os.path.isfile(f'LAW/{file_name}'):
+                continue
+            with open(f'LAW/{file_name}', 'w', encoding='utf-8') as f:
+                json.dump(data, f, ensure_ascii=False, indent=4)
+            # print(file_name)
             # Do anything you want here!!!
 
         ptt_bot.logout()
 
 
 if __name__ == '__main__':
-    board = 'Gossiping'
-    test_range = 100
+    board = 'LAW'
+    end_index = 32598
 
-    end_index = 779306
-
-    start_index = end_index - test_range + 1
+    start_index = 1
 
     import time
 
@@ -92,4 +150,4 @@ if __name__ == '__main__':
 
     total_time = int(end_time - start_time)
 
-    print(f'爬行 {board} 共 {test_range} 篇文章 共耗費時間 %.2f 秒' % total_time)
+    print(f'爬行 {board} 共耗費時間 %.2f 秒' % total_time)
